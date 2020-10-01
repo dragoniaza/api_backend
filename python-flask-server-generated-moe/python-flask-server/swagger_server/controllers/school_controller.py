3a1372659fbeb262b8db49340d9d1627bdccfdbc
dragoniaza <31649097+dragoniaza@users.noreply.github.com>
2020-10-01 14:24:10 +0700

    Add files via upload

---
+++ python-flask-server-generated-moe/python-flask-server/swagger_server/controllers/school_controller.py
@@ -0,0 +1,100 @@
+import connexion
+import six
+
+from swagger_server.models.school1 import School1  # noqa: E501
+from swagger_server.models.school2 import School2  # noqa: E501
+from swagger_server.models.school3 import School3  # noqa: E501
+from swagger_server.models.school4 import School4  # noqa: E501
+from swagger_server.models.school5 import School5  # noqa: E501
+from swagger_server.models.school6 import School6  # noqa: E501
+from swagger_server import util
+
+
+def school_jurisdictionname(key=None, api_key=None):  # noqa: E501
+    """jurisdictionname
+
+     # noqa: E501
+
+    :param key: set specify jurisdictionname of data
+    :type key: str
+    :param api_key: an authorization key
+    :type api_key: str
+
+    :rtype: School5
+    """
+    return 'do some magic!'
+
+
+def school_schoolregion(key=None, api_key=None):  # noqa: E501
+    """schoolregion
+
+     # noqa: E501
+
+    :param key: set specify schoolregion of data
+    :type key: str
+    :param api_key: an authorization key
+    :type api_key: str
+
+    :rtype: School6
+    """
+    return 'do some magic!'
+
+
+def schoolmoe_organizationtypename(key=None, api_key=None):  # noqa: E501
+    """organizationtypename
+
+     # noqa: E501
+
+    :param key: set specify organizationtypename of data
+    :type key: str
+    :param api_key: an authorization key
+    :type api_key: str
+
+    :rtype: School4
+    """
+    return 'do some magic!'
+
+
+def schoolmoe_provincename(key=None, api_key=None):  # noqa: E501
+    """provincename
+
+     # noqa: E501
+
+    :param key: set specify provincename of data
+    :type key: str
+    :param api_key: an authorization key
+    :type api_key: str
+
+    :rtype: School3
+    """
+    return 'do some magic!'
+
+
+def schoolmoe_schoolname(key=None, api_key=None):  # noqa: E501
+    """schoolname
+
+     # noqa: E501
+
+    :param key: set specify schoolname of data
+    :type key: str
+    :param api_key: an authorization key
+    :type api_key: str
+
+    :rtype: School1
+    """
+    return 'do some magic!'
+
+
+def schoolmoe_schoolnameeng(key=None, api_key=None):  # noqa: E501
+    """schoolnameeng
+
+     # noqa: E501
+
+    :param key: set specify schoolnameeng of data
+    :type key: str
+    :param api_key: an authorization key
+    :type api_key: str
+
+    :rtype: School2
+    """
+    return 'do some magic!'
